a0696ea67ff7c903a172f6bffc182ea4b7c9f71d
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-06-02 13:40:45 +0300

    Инициализация серверной части

---
+++ documenti-backend/build.gradle.kts
@@ -0,0 +1,75 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+project.extra.set("versions", mapOf(
+        "mybatis" to "2.0.1",
+        "mapstruct" to "1.3.0.Final",
+        "postgresql" to "42.2.5",
+        "liquibase" to "3.6.3",
+        "jackson" to "2.9.9",
+        "kotlin" to "1.2.71"
+))
+
+plugins {
+    id("org.springframework.boot") version "2.1.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.7.RELEASE"
+
+    kotlin("jvm") version "1.2.71"
+    kotlin("plugin.spring") version "1.2.71"
+    kotlin("kapt") version "1.2.71"
+}
+
+val versions : Map<String, String> by project.extra
+
+group = "com.romanidze"
+version = "0.0.1"
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    gradlePluginPortal()
+}
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-quartz")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${versions["jackson"]}")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${versions["kotlin"]}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${versions["kotlin"]}")
+
+    implementation("org.liquibase:liquibase-core:${versions["liquibase"]}")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:${versions["mybatis"]}")
+
+    runtimeOnly("org.postgresql:postgresql:${versions["postgresql"]}")
+
+    compile("org.mapstruct:mapstruct:${versions["mapstruct"]}")
+    kapt("org.mapstruct:mapstruct-processor:${versions["mapstruct"]}")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
