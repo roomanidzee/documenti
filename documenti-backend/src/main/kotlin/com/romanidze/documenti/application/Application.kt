4940fef30bbbe7395681cdb68d876862029f8836
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-06-30 18:45:11 +0300

    Исправил инициализацию batch - таблиц

--- documenti-backend/src/main/kotlin/com/romanidze/documenti/application/Application.kt
+++ documenti-backend/src/main/kotlin/com/romanidze/documenti/application/Application.kt
@@ -1,8 +1,10 @@
 package com.romanidze.documenti.application
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.romanidze.documenti.batch",
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.ComponentScan
                                "com.romanidze.documenti.controllers",
                                "com.romanidze.documenti.mappers",
                                "com.romanidze.documenti.services"])
+@EnableBatchProcessing
+@EnableScheduling
 class Application
 
 fun main(args: Array<String>) {
